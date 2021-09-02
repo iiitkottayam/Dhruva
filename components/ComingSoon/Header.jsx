6ed7079e79beecc9fc490c8038624e1eb7da653a
Sarthak Bindal <78961306+bindalsarthak03@users.noreply.github.com>
2021-09-02 22:05:46 +0530

    Responsiveness Added

--- components/ComingSoon/Header.jsx
+++ components/ComingSoon/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
     return (
         <>
             <div className={styles.hbody}>
-                <p className={styles.heading}><span>Create jaw dropping virtual</span> and hybrid events</p>
+                <p className={styles.heading}><span>Create jaw-dropping virtual</span> & hybrid events</p>
                 <p className={styles.desciption}>Welcome is the all-in-one virtual and hybrid<br /> event platform to power incredible<br /> experiences</p>
                 <HButtons />
 
--- pages/index.js
+++ pages/index.js
@@ -13,26 +13,7 @@ export default function Home() {
                     content="Dhruva IIITK Technical Fest"
                 />
                 <link rel="icon" href="/favicon.ico" />
-                <link rel="preconnect" href="https://fonts.googleapis.com" />
-                <link
-                    rel="preconnect"
-                    href="https://fonts.gstatic.com"
-                    crossOrigin
-                />
-                <link
-                    href="https://fonts.googleapis.com/css2?family=Lexend+Deca:wght@100&display=swap"
-                    rel="stylesheet"
-                />
-                <link rel="preconnect" href="https://fonts.googleapis.com" />
-                <link
-                    rel="preconnect"
-                    href="https://fonts.gstatic.com"
-                    crossOrigin
-                />
-                <link
-                    href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100&display=swap"
-                    rel="stylesheet"
-                />
+                
             </Head>
             <Header />
         </div>
