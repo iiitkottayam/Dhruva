f9a72b47a33bef4fc0e543e2d0c90acdb04e338b
shashank1503-cipher <54381338+shashank1503-cipher@users.noreply.github.com>
2021-09-04 12:22:52 +0530

    Formating Done

--- components/EventPage/EventHeading.js
+++ components/EventPage/EventHeading.js
@@ -1,19 +1,17 @@
 import styles from './EventHeading.module.css'
 import Image from 'next/image'
 import eventPoster from '../../public/Design006_QZ3bXyTwaT.jpg'
-import HButton from "@components/Accessories/button";
+import HButton from '@components/Accessories/button'
 const EventHeading = (props) => {
     return (
         <div className={styles.container}>
             <div className={styles.Heading}>
                 <h1>{props.title}</h1>
-                <p>
-                    {props.content}
-                </p>
+                <p>{props.content}</p>
                 <HButton name="Register"></HButton>
             </div>
-            <div className = {styles.image}>
-            <Image src={eventPoster} alt="Event Poster" priority={true} />
+            <div className={styles.image}>
+                <Image src={eventPoster} alt="Event Poster" priority={true} />
             </div>
         </div>
     )
