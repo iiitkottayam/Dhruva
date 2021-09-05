4d00506df827c6cabf2333291d2ec0259db2b762
Ishaan Mahesh <ishaanmahesh20bcs54@iiitkottayam.ac.in>
2021-09-05 23:22:35 +0530

    Merged Sponsor_FAQ section with the latest dev branch

---
+++ components/Sponsor_Faq/Sponsor_Faq.jsx
@@ -0,0 +1,36 @@
+import Style from './FAQ.module.css'
+import HButton from '@components/Accessories/button'
+import {ImArrowUpRight2} from 'react-icons/im'
+
+const Sponsor_Faq = () => {
+    return (
+        <div className = {Style.wrapper} id = {"FAQ"}>          {/* in-page navigation to FAQ section on clicking FAQ from the navbar */} {/* also added smooth scroll for the entire page in globals.css */}
+            <div className = {Style.backgrounds}>               {/* A layer containing the background for the sponsor and faq section, placed below the content using a negative z-index */}
+                <div className = {Style.SponsorBack}></div>
+                <div className = {Style.FaqBack}></div>
+            </div>  
+            <div className = {Style.content}>               {/* Contains all the content displayed on the FAQ component */}              
+                <div className = {Style.sponsor}>
+                    <p>Want to be a Sponsor?</p>
+                    <div className = {Style.ButtonWrapper}><a href="#"><HButton name = {"Apply"} icon = {<ImArrowUpRight2/>}/></a></div>    {/* Included an additional attribute - icon for the up-left arrow, also made the required change in button.jsx */}
+                </div>
+                <div className = {Style.contacts}>
+                    <div className = {Style.faq}> 
+                        <span>FAQ</span>
+                        <p className = {Style.subtitle}>Having some doubt?<br></br>Reach out to us</p>
+                        <a href="#"><HButton name = {"Contact us"} icon = {<ImArrowUpRight2/>}/></a>
+                    </div>
+                    <div className = {Style.chat}>  
+                        <div className = {Style.chatW}>Lorem ipsum dolor sit amet, ?</div>
+                        <div className = {Style.chatB}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,</div>
+                        <div className = {Style.chatW}>Lorem ipsum dolor sit amet, ?</div>
+                        <div className = {Style.chatW}>Lorem ipsum dolor sit amet, ?</div>
+                        <div className = {Style.chatW}>Lorem ipsum dolor sit amet, ?</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Sponsor_Faq
