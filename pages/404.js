c06b7022f3a15ab54ab91a4dee9686ee259079c6
Lokesh Rai <lokeshrai2019@iiitkottayam.ac.in>
2021-09-07 11:35:02 +0530

    changes-updated

--- pages/404.js
+++ pages/404.js
@@ -11,7 +11,7 @@ const error = () => {
 				<h1>4<span>0</span>4</h1>
 			</div>
 			<p>The page you are looking for might have been removed or is temporarily unavailable.</p>
-			<a href="/"><HButton name="Home Page" icon = {<ImArrowUpRight2/>}/></a>
+			<a className="sponsor" href="/"><HButton  name="Home Page" icon = {<ImArrowUpRight2/>}/></a>
 		</div>
 	    </div>
     )
