509bcac3ce64e03329f8d313ef4ad5d488815061
faiz-hasan11 <faizhasan2019@iiitkottayam.ac.in>
2021-09-10 14:39:58 +0530

    Team Inline CSS

--- components/Team/Card.js
+++ components/Team/Card.js
@@ -8,7 +8,7 @@ import {
 } from 'react-icons/fa'
 import AOS from "aos";
 import "aos/dist/aos.css";
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 const Card = () => {
         useEffect(() => {
@@ -16,10 +16,23 @@ const Card = () => {
           // duration : 5000
         });
       }, []);
-    return (
-        <div className={styles.container}  data-aos="fade-up" data-aos-duration="1500">
 
-                <div className={styles.profile_img}></div>
+      const [img,setImg] = useState("url(https://images.unsplash.com/photo-1497316730643-415fac54a2af?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1900&q=80)");
+
+    return (
+        <div 
+          className={styles.container} 
+          onMouseOver={() => setImg("url(https://c.tenor.com/DHGvsLhTOowAAAAC/meme-pikachu.gif)")} 
+          onMouseOut={() =>setImg("url(https://images.unsplash.com/photo-1497316730643-415fac54a2af?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1900&q=80)")}
+          data-aos="fade-up" 
+          data-aos-duration="1500"
+        >
+                <div 
+                  className={styles.profile_img}
+                  style={{
+                          backgroundImage:img,                          
+                        }}
+                ></div>
                 <div className={styles.content_container}>
                     <span className={styles.name}>Syed Faiz Hasan</span>
                     <span className={styles.role}>Frontend Developer</span>
