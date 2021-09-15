ff7d617b0cc23e7b019d2f80f7be4bd8617027d5
Sarthak Bindal <78961306+bindalsarthak03@users.noreply.github.com>
2021-09-15 12:17:29 +0530

    Icon size decreased

--- components/Events/Card.jsx
+++ components/Events/Card.jsx
@@ -1,29 +1,29 @@
 import styles from '/styles/Orgnjdge.module.css'
-import { TiSocialLinkedinCircular, TiSocialTwitterCircular, TiSocialFacebookCircular } from 'react-icons/ti'
+import { FaLinkedin, FaTwitter, FaFacebook } from 'react-icons/Fa'
 import Link from 'next/dist/client/link'
 
 const Card = (props) => {
     return (
         <div className={styles.card}>
             <div className={styles.cimg}>
-                <img src={props.imgsrc} alt="Organiser"  />
+                <img src={props.imgsrc} alt="Organiser" />
                 <p className={styles.name}>{props.name}</p>
             </div>
             <div className={styles.socicons}>
                 <Link href={props.link1} >
                     <a target="_blank">
-                        <TiSocialLinkedinCircular className={styles.taicon}  />
+                        <FaLinkedin className={styles.faicon} />
                     </a>
 
                 </Link>
                 <Link href={props.link2} >
                     <a target="_blank">
-                        <TiSocialFacebookCircular className={styles.taicon}  />
+                        <FaFacebook className={styles.faicon} />
                     </a>
                 </Link>
                 <Link href={props.link3} >
                     <a target="_blank">
-                        <TiSocialTwitterCircular className={styles.taicon}  />
+                        <FaTwitter className={styles.faicon} />
                     </a>
                 </Link>
             </div>
