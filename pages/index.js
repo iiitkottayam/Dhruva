e08be66ce4862e211b93e02d33e6a9842cd29430
Hrishabh Joshi <harishabh2019@iiitkottayam.ac.in>
2021-09-20 11:49:16 +0530

    SEO updates

--- pages/index.js
+++ pages/index.js
@@ -13,26 +13,42 @@ import Sponsor_Faq from '@components/Sponsor_Faq/Sponsor_Faq'
 import Attendes from '@components/AttendeesSpeakers/attendes'
 
 export default function Home() {
-  return (
-    <>
-    <div className={styles.container}>
-      <Head>
-        <title>DHRUVA - IIITK</title>
-        <meta name="description" content="Dhruva IIITK Technical Fest" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <Navbar/>
-      <Header />
-      <About/> 
-      <Attendes />
-      <Speakers />
-      <Timeline />
-      <Sponsors/>
+    return (
+        <>
+            <div className={styles.container}>
+                <Head>
+                    <title>DHRUVA - IIITK</title>
+                    <meta
+                        name="description"
+                        content="Dhruva Indian Institute of Information Technology IIIT Kottayam Technical Fest"
+                    />
+                    <meta
+                        property="og:title"
+                        content="Dhruva Technical Fest IIIT Kottayam"
+                    />
+                    <meta
+                        property="og:description"
+                        content="Dhruva - Indian Institute of Information Technology (IIIT) Kottayam Technical Fest"
+                    />
+                    <meta
+                        property="og:url"
+                        content="https://www.dhruvaiiitk.tech/"
+                    />
+                    <meta property="og:type" content="website" />
+                    <link rel="icon" href="/favicon.ico" />
+                </Head>
+                <Navbar />
+                <Header />
+                <About />
+                <Attendes />
+                <Speakers />
+                <Timeline />
+                <Sponsors />
 
-      <Sponsor_Faq />
-    </div>
-    
-    <Footer></Footer>
-    </>
-  )
+                <Sponsor_Faq />
+            </div>
+
+            <Footer></Footer>
+        </>
+    )
 }
