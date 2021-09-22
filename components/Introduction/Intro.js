b2a6d3c53fbf3c2ce6461a7228ed03442603209a
Aryan Yadav <aryan2019@iiitkottayam.ac.in>
2021-09-22 15:04:08 +0530

    Update Intro.js

--- components/Introduction/Intro.js
+++ components/Introduction/Intro.js
@@ -4,17 +4,14 @@ const Intro = () => {
     return (
         <div className={styles.container}>
             <div className={styles.textWrapper}>
-                <div className={`${styles.text1} ${styles.text}`}>Dhruva</div>
-                <div className={`${styles.text2} ${styles.text}`}>Dhruva</div>
-                <div className={`${styles.text3} ${styles.text}`}>Dhruva</div>
-                <div className={`${styles.text4} ${styles.text}`}>Dhruva</div>
-                <div className={`${styles.text5} ${styles.text}`}>Dhruva</div>
-                <div className={`${styles.text6} ${styles.text}`}>Dhruva</div>
-                <div className={`${styles.text7} ${styles.text}`}>Dhruva</div>
-                <div className={`${styles.text8} ${styles.text}`}>Dhruva</div>
-                <div className={`${styles.text9} ${styles.text}`}>Dhruva</div>
-                <div className={`${styles.text10} ${styles.text}`}>Dhruva</div>
-                <div className={`${styles.text11} ${styles.text}`}>Dhruva</div>
+                <div className={`${styles.text3} ${styles.text}`}>DHRUVA</div>
+                <div className={`${styles.text4} ${styles.text}`}>DHRUVA</div>
+                <div className={`${styles.text5} ${styles.text}`}>DHRUVA</div>
+                <div className={`${styles.text6} ${styles.text}`}>DHRUVA</div>
+                <div className={`${styles.text7} ${styles.text}`}>DHRUVA</div>
+                <div className={`${styles.text8} ${styles.text}`}>DHRUVA</div>
+                <div className={`${styles.text9} ${styles.text}`}>DHRUVA</div>
+                
             </div>
         </div>
     )
