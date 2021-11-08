f7d58e423e4ec88a04c574603ea1e82de6aa323d
Ishaan Mahesh <ishaanmahesh20bcs54@iiitkottayam.ac.in>
2021-11-08 14:32:03 +0530

    leaderboard responsiveness changes , 3 more links included

--- components/Leaderboard_Table/Board.jsx
+++ components/Leaderboard_Table/Board.jsx
@@ -1,21 +1,32 @@
-import { useRouter } from 'next/router'
-import { useState, useEffect } from 'react'; 
-import { MDBContainer, MDBIframe } from "mdbreact";
+import { useRouter } from 'next/router';
+import { useState, useEffect } from 'react';
+import IframeResizer from 'iframe-resizer-react';
 import Loader from "react-loader-spinner";
 import styles from "./Leaderboard.module.css";
 
 const Board = () => {
 
     const [load, setLoad] = useState(true);
+    var frameHeight = 0;
+    
+    const handleResize = () => {
+        var frame = document.getElementById("dynamic");
+        frameHeight = frameHeight == 0 ? frame.style.getPropertyValue("height") : frameHeight;
+ 
+        if (window.innerWidth >= 900) {
+            frame.style.setProperty("height", frameHeight.toString(), "important")
+        }
+    }
 
     useEffect(() => {
         setTimeout(() => {
             setLoad(false);
-        },2000);
+        },2500);
+
+        window.addEventListener("resize", handleResize);
     });
 
     const router = useRouter();
-    const docSheet = "https://opensheet.vercel.app/" + router.query["val"] + "/Sheet1";
     const title = router.query["nam"];
     
     return (
@@ -24,9 +35,12 @@ const Board = () => {
             <h4 className = {styles.hdn2}>Leaderboard</h4>
             {load ? <Loader color="#8c58fc" type="Puff"/> 
             : 
-                <MDBContainer className="text-center">
-                    <MDBIframe src = {"https://ishaan5199.github.io/Data-Table/?val=" + router.query["val"]}/>
-                </MDBContainer>
+                <IframeResizer
+                    src = {"https://ishaan5199.github.io/Data-Table/?val=" + router.query["val"]}
+                    style = {{ width: '1px', minWidth: '90%', marginBottom: '5rem'}}
+                    frameBorder = "0"
+                    id = "dynamic"
+                />
             }
         </>
         
